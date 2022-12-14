16de3d25e9632d2c8d22b75ed88578479b887bee
terria1020 <jaehan1346@gmail.com>
2022-12-14 14:40:45 +0900

    [feature] 입력받은 월의 최대 일수 출력하기

--- src/terria1020/calender/Calender.java
+++ src/terria1020/calender/Calender.java
@@ -4,16 +4,18 @@ import java.util.Scanner;
 
 public class Calender {
     public static void main(String[] args) {
-        int number1;
-        int number2;
-        Scanner sc;
+        int month;
+        int last;
+        Scanner sc = new Scanner(System.in);
 
-        sc = new Scanner(System.in);
+        System.out.println("달을 입력하세요: ");
 
-        System.out.println("두 수를 입력하세요:");
-        number1 = sc.nextInt();
-        number2 = sc.nextInt();
+        month = sc.nextInt();
 
-        System.out.println("두 수의 합은 " + (number1 + number2) + "입니다.");
-    }
+        if (month == 2) last = 28;
+        else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) last = 31;
+        else last = 30;
+
+        System.out.println(month + "월은 " + last + "일까지 있습니다.");
+     }
 }
