26770e9523e12f2baa183c0d5667460edfac41c2
terria1020 <jaehan1346@gmail.com>
2022-12-14 14:57:38 +0900

    [update] 입력받은 월의 최대 일수 출력하기 해답편 실습

--- src/terria1020/calender/Calender.java
+++ src/terria1020/calender/Calender.java
@@ -3,19 +3,24 @@ package terria1020.calender;
 import java.util.Scanner;
 
 public class Calender {
+
+    private static final int[] MAX_DAYS = {
+            31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
+    };
+
+    public int getLastDaysOfMonth(int month) {
+        return MAX_DAYS[month - 1];
+    }
+
     public static void main(String[] args) {
         int month;
-        int last;
         Scanner sc = new Scanner(System.in);
+        Calender calender = new Calender();
 
         System.out.println("달을 입력하세요: ");
 
         month = sc.nextInt();
 
-        if (month == 2) last = 28;
-        else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) last = 31;
-        else last = 30;
-
-        System.out.println(month + "월은 " + last + "일까지 있습니다.");
-     }
+        System.out.println(month + "월은 " + calender.getLastDaysOfMonth(month) + "일까지 있습니다.");
+    }
 }
