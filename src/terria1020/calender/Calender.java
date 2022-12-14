931f65aa11b0465d53a2a7c0c52128b3a1410e42
terria1020 <jaehan1346@gmail.com>
2022-12-14 13:55:51 +0900

    println 사용해서 출력하기

--- src/terria1020/calender/Calender.java
+++ src/terria1020/calender/Calender.java
@@ -2,6 +2,12 @@ package terria1020.calender;
 
 public class Calender {
     public static void main(String[] args) {
-        System.out.println("Hello, Calender");
+        System.out.println("일 월 화 수 목 금 토");
+        System.out.println("---------------------");
+        System.out.println(" 1 2 3 4 5 6 7");
+        System.out.println(" 8 9 10 11 12 13");
+        System.out.println(" 14 15 16 17 18 19");
+        System.out.println(" 20 21 22 23 24 25");
+        System.out.println(" 26 27 28 29 30 31");
     }
 }
