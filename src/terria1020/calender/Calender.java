6bbabdec382899c11baf173dda689fa81f4c46ce
terria1020 <jaehan1346@gmail.com>
2022-12-14 15:53:38 +0900

    [update] 종료 조건을 이용해 반복하게 변경

--- src/terria1020/calender/Calender.java
+++ src/terria1020/calender/Calender.java
@@ -8,34 +8,35 @@ public class Calender {
             31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
     };
 
+    private static final String PROMPT = "> ";
+
     public int getLastDaysOfMonth(int month) {
         return MAX_DAYS[month - 1];
     }
 
+    public static int getMonth() {
+        System.out.println("달을 입력하세요: ");
+        System.out.print(PROMPT);
+
+        Scanner sc = new Scanner(System.in);
+        return sc.nextInt();
+    }
+
     public static void main(String[] args) {
-        int[] months;
-        int[] maxDays;
+        int month;
+        int maxDay;
         int count;
         Scanner sc = new Scanner(System.in);
         Calender calender = new Calender();
 
-        System.out.println("반복 횟수를 입력하세요:");
-        count = sc.nextInt();
-        sc.nextLine();
+        while (true) {
+            month = getMonth();
+            if (month == -1) break;
 
-        months = new int[count];
-        maxDays = new int[count];
-        for (int i = 0; i < count; i++) {
-            System.out.println("달을 입력하세요: ");
-
-            months[i] = sc.nextInt();
-            sc.nextLine();
-
-            maxDays[i] = calender.getLastDaysOfMonth(months[i]);
+            maxDay = calender.getLastDaysOfMonth(month);
+            System.out.println(month + "월은 " + maxDay + "일까지 있습니다.");
         }
 
-        for (int i = 0; i < count; i++) {
-            System.out.println(months[i] + "월은 " + maxDays[i] + "일까지 있습니다.");
-        }
+        System.out.println("Have a nice day!");
     }
 }
