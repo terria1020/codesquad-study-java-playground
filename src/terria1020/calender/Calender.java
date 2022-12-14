a26091a2fd29c28d977467ee0a8d6cd5b9e5ead3
terria1020 <jaehan1346@gmail.com>
2022-12-14 15:37:45 +0900

    [update] 반복 입력 가능하도록 프로그램 변경하기

--- src/terria1020/calender/Calender.java
+++ src/terria1020/calender/Calender.java
@@ -13,14 +13,29 @@ public class Calender {
     }
 
     public static void main(String[] args) {
-        int month;
+        int[] months;
+        int[] maxDays;
+        int count;
         Scanner sc = new Scanner(System.in);
         Calender calender = new Calender();
 
-        System.out.println("달을 입력하세요: ");
+        System.out.println("반복 횟수를 입력하세요:");
+        count = sc.nextInt();
+        sc.nextLine();
 
-        month = sc.nextInt();
+        months = new int[count];
+        maxDays = new int[count];
+        for (int i = 0; i < count; i++) {
+            System.out.println("달을 입력하세요: ");
 
-        System.out.println(month + "월은 " + calender.getLastDaysOfMonth(month) + "일까지 있습니다.");
+            months[i] = sc.nextInt();
+            sc.nextLine();
+
+            maxDays[i] = calender.getLastDaysOfMonth(months[i]);
+        }
+
+        for (int i = 0; i < count; i++) {
+            System.out.println(months[i] + "월은 " + maxDays[i] + "일까지 있습니다.");
+        }
     }
 }
