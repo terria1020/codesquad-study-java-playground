6b2effe41c106d2f572e00ff1cb3a65a81cca2ff
terria1020 <jaehan1346@gmail.com>
2022-12-17 11:46:05 +0900

    [feature] 윤년 처리 로직 추가
    윤년 처리 로직 추가
    Deprecated 처리했던 함수 삭제
    year 변수 scope 변경

--- src/terria1020/calender/Calender.java
+++ src/terria1020/calender/Calender.java
@@ -8,14 +8,24 @@ public class Calender {
             31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31
     };
 
+    private static final int LEAP_DAY = 29;
+
+    private int year;
+
+    public Calender() {
+        year = LocalDateTime.now().getYear();
+    }
+
+    public boolean isLeapYear() {
+        return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
+    }
+
     public int getLastDaysOfMonth(int month) {
+        if (isLeapYear() && month == 2) return LEAP_DAY;
         return MAX_DAYS[month - 1];
     }
 
-
     public void printCalender(int month) {
-        int year = LocalDateTime.now().getYear();
-
         System.out.printf("\t<%4d년%3d월>", year, month);
         System.out.println();
         System.out.println(" 일  월  화  수  목  금  토");
@@ -28,14 +38,5 @@ public class Calender {
             if (i % 7 == 0) System.out.println();
         }
         System.out.println();
-    }
-
-    @Deprecated
-    public static int getMonth() {
-        System.out.println("달을 입력하세요: ");
-        System.out.print("> ");
-
-        Scanner sc = new Scanner(System.in);
-        return sc.nextInt();
     }
 }
