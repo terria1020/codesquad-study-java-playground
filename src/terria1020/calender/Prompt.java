e294f773bb9122dc2e432749b5f6bcfc17a974d3
terria1020 <jaehan1346@gmail.com>
2022-12-22 15:52:21 +0900

    [feature] 이번달 달력 출력, 프롬프트 출력 등
    Prompt.run() 변경: 메뉴 출력을 위한 코드 작성, 메뉴 별 메소드 분리 등.
    showTodayCalender 구현
    help 출력 구현
    일부 코드 변경, 구조 개선 등

--- src/terria1020/calender/Prompt.java
+++ src/terria1020/calender/Prompt.java
@@ -1,5 +1,6 @@
 package terria1020.calender;
 
+import java.time.LocalDateTime;
 import java.util.Scanner;
 
 public class Prompt {
@@ -11,9 +12,25 @@ public class Prompt {
     private Calender calender;
 
     private interface env {
+        String CONSOLE = "+================+" +
+                "\n| 1. 일정 등록" +
+                "\n| 2. 일정 검색" +
+                "\n| 3. 달력 보기" +
+                "\n| 4. 달력 검색" +
+                "\n| h. 도움말" +
+                "\n| q. 종료" +
+                "\n+================+" +
+                "\n명령: (1, 2, 3, 4, h, q)";
         String PS1 = "cal > ";
         String PS2 = "year > ";
         String PS3 = "month > ";
+
+        String HELP = "1. 특정 날짜에 일정을 등록합니다." +
+                "\n2. 특정 날짜에 일정이 등록되어 있는지 검색합니다." +
+                "\n3. 현재 날짜의 이번 달 달력을 출력합니다." +
+                "\n4. 특정 연도의 특정 달 달력을 검색합니다." +
+                "\nh. 도움말을 출력합니다." +
+                "\nq. 프로그램을 종료합니다.\n";
     }
 
     public Prompt() {
@@ -22,31 +39,61 @@ public class Prompt {
     }
 
     public void run() {
+        char menu;
         while (true) {
-            System.out.println("년도를 입력하세요.");
-            System.out.print(env.PS2);
-            year = input();
-            if (year == -1) break;
+            System.out.println(env.CONSOLE);
+            System.out.print(env.PS1);
+            menu = scanner.nextLine().charAt(0);
 
-            System.out.println("달을 입력하세요");
-            System.out.print(env.PS3);
-            month = input();
-
-            if (month == -1) break;
-            else if (month < 1 || month > 12) continue;
-
-            calender.printCalender(year, month);
+            if (menu == '1') scheduleSign();
+            else if (menu == '2') searchSchedule();
+            else if (menu == '3') showTodayCalender();
+            else if (menu == '4') searchCalender();
+            else if (menu == 'h') printHelp();
+            else if (menu == 'q') break;
         }
         printMotd();
+    }
+
+    public void scheduleSign() {
+
+    }
+
+    public void searchSchedule() {
+
+    }
+
+    public void showTodayCalender() {
+        LocalDateTime now = LocalDateTime.now();
+        calender.printCalender(now.getYear(), now.getMonthValue());
+    }
+
+    public void searchCalender() {
+        System.out.println("년도를 입력하세요.");
+        System.out.print(env.PS2);
+        year = input();
+        if (year == -1) return;
+
+        System.out.println("달을 입력하세요");
+        System.out.print(env.PS3);
+        month = input();
+
+        if (month == -1 || month < 1 || month > 12) return;
+
+        calender.printCalender(year, month);
+    }
+
+    public void printHelp() {
+        System.out.println(env.HELP);
+    }
+
+    public void printMotd() {
+        System.out.println("Bye-!");
     }
 
     public int input() {
         int inputNumber = scanner.nextInt();
         scanner.nextLine();
         return inputNumber;
-    }
-
-    public void printMotd() {
-        System.out.println("Bye-!");
     }
 }
